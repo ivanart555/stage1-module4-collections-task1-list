593301e730859c7ab45f2697780a36f54679af00
Ivan Artsiushyn <ivanart555@gmail.com>
2023-07-20 12:53:37 +0300

    Solve tasks

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,16 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> result = new LinkedList<>();
+
+        for (Integer number : sourceList) {
+
+            if (number % 2 == 0) {
+                result.addLast(number);
+            } else result.addFirst(number);
+
+        }
+
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,18 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int first = Integer.parseInt(a);
+        int second = Integer.parseInt(b);
+
+        if (Math.abs(first) == Math.abs(second)) return first - second;
+
+        return Math.abs(5 * first ^ 2 + 3) - Math.abs(5 * second ^ 2 + 3);
     }
 }
